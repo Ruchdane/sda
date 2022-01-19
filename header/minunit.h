8be8c081d7693157208dfd505e09419ad6716595
Ruchdane <ruchdaneabiodun@gmail.com>
2022-01-19 14:01:00 +0100

    minunit update

--- header/minunit.h
+++ header/minunit.h
@@ -4,15 +4,15 @@
 #define mu_assert(test, message) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; \
 								if (message) {\
-										tests_failed++; printf("Test failed because %s\n\n",message);}\
+										tests_failed++; printf("\033[37;41mTest failed because %s\033[0m\n\n",message);}\
 								else{\
-									tests_passed++,printf("Test passed\n\n");} } while (0)
+									tests_passed++,printf("\033[37;42mTest passed\033[0m\n\n");} } while (0)
 #define mu_begin_of_a_test() printf("Testing %s\n",__func__);
 extern int tests_run;
 extern int tests_failed;
 extern int tests_passed;
 #define mu_begin() tests_run = 0;tests_failed = 0;tests_passed = 0;
-#define mu_result() printf("End of tests \nTest run: %d\nTest Failed: %d\nTest passed %d\n",tests_run,tests_failed,tests_passed);
+#define mu_result() printf("End of tests \nTest run: %d\nTest Failed: \033[31m%d\033[0m\nTest passed \033[32m%d\033[0m\n",tests_run,tests_failed,tests_passed);
 #endif // MINUNITMODIFIEDBYRUCHDANE
 
 /* Template
